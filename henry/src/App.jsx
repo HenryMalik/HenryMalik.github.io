792b54e5fc8addfdd9ec7b0c50633de04e8b9c49
HenryMalik <henrymalik03@gmail.com>
2023-01-21 16:28:14 +0700

    first build

--- henry/src/App.jsx
+++ henry/src/App.jsx
@@ -4,29 +4,47 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const arraynama = ['ilyas', 'yusuf', 'henry', 'budi'];
+  const arrayNumber = [1,2,3,4,5,6,7,8,9,10,11,12]
+  //use state dalam react berguna untuk menyimpan variable
+
+  const [arraySearch,setArraySearch] = useState(arraynama)
+  const [numbersearch,setnumbers] = useState(arrayNumber)
+  
+
+  function oninputberubah(data){
+    const text = data.target.value;
+    const hasilSearch = arrayNumber.filter((item) =>{
+      return item > text
+    })
+    setnumbers(hasilSearch)
+  }
+
+  const tambahNilai = ()=>{
+    setCount(count + 1)
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + {count}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={tambahNilai}>
+          tambah nilai
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input type="text" onChange={oninputberubah} />
+        <div style={{
+          display:'flex',
+          flexDirection:'row'
+        }}>
+          {numbersearch.map((item)=>{
+            return (
+              <h3>{item} ,</h3>
+            )
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
