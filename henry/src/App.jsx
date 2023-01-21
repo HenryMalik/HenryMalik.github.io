5b5976f5aabe8ad211f449ec4eb5e4f87038733f
hikaaam <andrenuryana@gmail.com>
2023-01-21 17:01:19 +0700

    update comments

--- henry/src/App.jsx
+++ henry/src/App.jsx
@@ -1,52 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  const arraynama = ['ilyas', 'yusuf', 'henry', 'budi'];
-  const arrayNumber = [1,2,3,4,5,6,7,8,9,10,11,12]
+  const [count, setCount] = useState(0);
+
+  const arraynama = ["ilyas", "yusuf", "henry", "budi"];
+  const arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   //use state dalam react berguna untuk menyimpan variable
 
-  const [arraySearch,setArraySearch] = useState(arraynama)
-  const [numbersearch,setnumbers] = useState(arrayNumber)
-  
+  const [arraySearch, setArraySearch] = useState(arraynama);
+  const [numbersearch, setnumbers] = useState(arrayNumber);
 
-  function oninputberubah(data){
-    const text = data.target.value;
-    const hasilSearch = arrayNumber.filter((item) =>{
-      return item > text
-    })
-    setnumbers(hasilSearch)
+  function oninputberubah(data) {
+    const text = data.target.value; //mengambil nilai input dari user
+    const hasilSearch = arrayNumber.filter((item) => {
+      //memfilter "arrayNumber" yang memiliki nilai lebih besar dari inputan user
+      return item > text;
+    });
+    setnumbers(hasilSearch); //hasil dari filter dimasukan ke dalam state "numbersearch" agar terupdate secara realtime
   }
 
-  const tambahNilai = ()=>{
-    setCount(count + 1)
-  }
+  const tambahNilai = () => {
+    setCount(count + 1); //mengupdate state "count" secara realtime
+  };
 
   return (
     <div className="App">
-      <h1>Vite + {count}</h1>
+      <h3>Nilai = {count}</h3>
       <div className="card">
-        <button onClick={tambahNilai}>
-          tambah nilai
-        </button>
-        <input type="text" onChange={oninputberubah} />
-        <div style={{
-          display:'flex',
-          flexDirection:'row'
-        }}>
-          {numbersearch.map((item)=>{
+        <button onClick={tambahNilai}>tambah nilai</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <h3>Masukan angka untuk memfilter array</h3>
+        <input
+          style={{ width: "100%" }}
+          type="text"
+          onChange={oninputberubah} //untuk memanggil function tiap input berubah
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "flex-start",
+            width: "100%",
+          }}
+        >
+          {numbersearch.map((item, index) => {
             return (
-              <h3>{item} ,</h3>
-            )
+              <h3>
+                {item}
+                {index < numbersearch.length - 1 ? "," : ""}
+              </h3>
+            );
           })}
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
