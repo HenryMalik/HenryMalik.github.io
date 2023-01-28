d41e2c812ca74757439d973899deae84f72a6f50
hikaaam <andrenuryana@gmail.com>
2023-01-28 17:05:57 +0700

    fix and build

--- henry/src/screens/HomePage/index.jsx
+++ henry/src/screens/HomePage/index.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 
 function App() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="increment">Increment</Link>
-            <Link to="persegi">LuasPersegi</Link>
-            <Link to="segitiga">LuasSegitiga</Link>
-            <Link to="filter">FIlterArray</Link>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Home Page</h1>
+      <Link to="increment">Increment</Link>
+      <Link to="persegi">LuasPersegi</Link>
+      <Link to="segitiga">LuasSegitiga</Link>
+      <Link to="filter">FIlterArray</Link>
+    </div>
+  );
 }
-
 
 export default App;
