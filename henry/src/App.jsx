8c8baa0c1ee0f241ed3945f5a9ab76dd8a3155e5
HenryMalik <henrymalik03@gmail.com>
2023-01-28 15:53:36 +0700

    luas persegi & segitiga

--- henry/src/App.jsx
+++ henry/src/App.jsx
@@ -13,6 +13,13 @@ function App() {
   const [numbersearch, setnumbers] = useState(arrayNumber);
   const [hitungsearch, setHitung] = useState(0)
 
+  const [inputpersegi, setinputpersegi] = useState("")
+  const [hasilpersegi, sethasilpersegi] = useState()
+
+  const [tinggi, settinggi] = useState("")
+  const [alas, setalas] = useState("")
+  const [hasiltiga, sethasiltiga] = useState()
+
   function oninputberubah(data) {
     const text = data.target.value; //mengambil nilai input dari user
     const hasilSearch = arrayNumber.filter((item) => {
@@ -21,12 +28,27 @@ function App() {
     });
     setnumbers(hasilSearch); //hasil dari filter dimasukan ke dalam state "numbersearch" agar terupdate secara realtime
   }
-  function oninputberhitung(data) {
+  function oninputpersegi(data) {
     const text = data.target.value;
-    const hasilHitung = "hasil dari luas persegi dengan sisi "+(text)+" adalah "+((item) => {
-      return text*text;
-    });
-   }
+    const hasilHitung = text * text;
+    setinputpersegi(text)
+    sethasilpersegi(hasilHitung)
+  }
+
+  function oninputalas(data) {
+    const text = data.target.value;
+    setalas(text)
+  }
+
+  function oninputtinggi(data) {
+    const text = data.target.value;
+    settinggi(text)
+  }
+
+  function hitungluassegitiga() {
+    const hasilhitung = (alas * tinggi) / 2
+    sethasiltiga(hasilhitung)
+  }
 
   const tambahNilai = () => {
     setCount(count + 1); //mengupdate state "count" secara realtime
@@ -69,23 +91,48 @@ function App() {
               </h3>
             );
           })}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "flex-star",
+            width: "100%",
+          }}
+        >
           <h3>masukan nilai untuk menghitung luas persegi</h3>
           <input
-            style= {{ width: "100%" }}
+            style={{ width: "100%" }}
             type="text"
-            onChange={oninputberhitung}
+            onChange={oninputpersegi}
           />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-evenly",
-              alignItems: "flex-star",
-              width: "100%",
-            }}
-          >
-            
-          </div>
+          {inputpersegi.length > 0 && <h4>hasil dari luas persegi dengan sisi {inputpersegi} adalah {hasilpersegi}</h4>}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "flex-star",
+            width: "100%",
+          }}
+        >
+          <h3>masukan nilai alas segitiga</h3>
+          <input
+            style={{ width: "100%" }}
+            type="text"
+            onChange={oninputalas}
+          />
+          <h3>masukan nilai tinggi segitiga</h3>
+          <input
+            style={{ width: "100%" }}
+            type="text"
+            onChange={oninputtinggi}
+          />
+          <button onClick={hitungluassegitiga}>hitung luas segitiga</button>
+          {alas.length > 0 && tinggi.length > 0 &&
+            <h4>hasil dari luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {hasiltiga}</h4>}
         </div>
       </div>
     </div>
