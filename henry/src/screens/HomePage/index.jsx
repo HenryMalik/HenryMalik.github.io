78fd4e2bd9932f87da323414f5ff1139dd3c387d
HenryMalik <henrymalik03@gmail.com>
2023-01-28 17:05:40 +0700

    tambah return

--- henry/src/screens/HomePage/index.jsx
+++ henry/src/screens/HomePage/index.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
 function App() {
-    <div>
-        <h1>Home Page</h1>
-        <Link to="increment">Increment</Link>
-        <Link to="persegi">LuasPersegi</Link>
-        <Link to="segitiga">LuasSegitiga</Link>
-        <Link to="filter">FIlterArray</Link>
-    </div>
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link to="increment">Increment</Link>
+            <Link to="persegi">LuasPersegi</Link>
+            <Link to="segitiga">LuasSegitiga</Link>
+            <Link to="filter">FIlterArray</Link>
+        </div>
+    );
 }
 
 
